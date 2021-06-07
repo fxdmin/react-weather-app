d8500f49fa7bfde54588cca29ab03d41a0c23421
Jeongmin-GO <go.jminn@gmail.com>
2021-06-07 19:16:12 +0900

    API 호출 : Getting the weather
    yarn add axios 또는 npm install axios 후 improt

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import {Alert} from "react-native";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "59fd839806a34ceafc609b2f8ffb328e";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async(latitude, longitude) => {
+      const { data } = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
+      console.log(data);
+  };
+
+  getLocation = async() => {
+    try{
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      // Send to API and get weather!
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading: false });
+    } catch(error){
+        Alert.alert("Can't find you.", "So sad");
+    }
+  };
+
+  componentDidMount(){
+    this.getLocation();
+  } 
+  render(){
+    const { isLoading } = this.state;
+    return isLoading? <Loading />: null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
